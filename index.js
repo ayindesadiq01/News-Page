4f23bcf48f67be36f0bea726a5d5901fa9ee3f54
ayindesadiq01 <ayindesadiq01@gmail.com>
2023-01-22 19:50:34 +0100

    new changes

--- index.js
+++ index.js
@@ -3,12 +3,17 @@ const hidden = document.querySelector('.hidden');
 const menu = document.querySelector('.menu');
 const closeMenu = document.querySelector('.close-menu');
 const navigate = document.querySelector('.navigate');
-const blur = document.querySelector('.blur');
+const overlay = document.querySelector('.overlay');
 
 
 menu.addEventListener('click', function(){
  navigate.classList.remove('hidden');
+ overlay.classList.remove('hidden');
 });
+
+
 closeMenu.addEventListener ('click', function(){
  navigate.classList.add('hidden');
+ overlay.classList.add('hidden');
+
 });
